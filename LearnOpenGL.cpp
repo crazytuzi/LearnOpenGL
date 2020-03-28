bec560fa28a7e00dda65ddcc340198c55890984c
crazytuzi <957824770@qq.com>
2020-03-29 01:02:06 +0800

    EBO

--- LearnOpenGL.cpp
+++ LearnOpenGL.cpp
@@ -127,16 +127,25 @@ int main()
 	/* set up vertex data (and buffer(s)) and configure vertex attributes */
 	// ------------------------------
 	float vertices[] = {
-		-0.5f, -0.5f, 0.f, /* left */
-		0.5f, -0.5f, 0.f, /* right */
-		0.f, 0.5f, 0.f /* top */
+		0.5f, 0.5f, 0.f, /* top right */
+		0.5f, -0.5f, 0.f, /*  bottom right */
+		-0.5f, -0.5f, 0.f, /* bottom left */
+		-0.5f, 0.5f, 0.f /* top left */
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {
+		/* note that we start from 0! */
+		0, 1, 3, /* first Triangle */
+		1, 2, 3 /* second Triangle */
+	};
+
+	unsigned int VBO, VAO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 
 	glGenBuffers(1, &VBO);
+
+	glGenBuffers(1, &EBO);
 
 	/* bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s). */
 	glBindVertexArray(VAO);
@@ -144,6 +153,10 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
 
@@ -157,6 +170,8 @@ int main()
 	 * VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	 */
 	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	/* render loop */
 	// ------------------------------
@@ -177,7 +192,7 @@ int main()
 		/* seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized */
 		glBindVertexArray(VAO);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.) */
 		// ------------------------------
@@ -191,6 +206,8 @@ int main()
 	glDeleteVertexArrays(1, &VAO);
 
 	glDeleteBuffers(1, &VBO);
+
+	glDeleteBuffers(1, &EBO);
 
 	/* glfw: terminate, clearing all previously allocated GLFW resources. */
 	// ------------------------------

1a2c8c927b0d10e285941178fe4f4f550c77825d
crazytuzi <957824770@qq.com>
2020-04-06 17:53:10 +0800

    Framebuffers

--- LearnOpenGL.cpp
+++ LearnOpenGL.cpp
@@ -4,10 +4,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
-#include <map>
 #include <stb_image.h>
-#include <vector>
-
 
 #include "Shader.h"
 #include "Camera.h"
@@ -90,78 +87,57 @@ int main()
 	// ------------------------------
 	glEnable(GL_DEPTH_TEST);
 
-	glEnable(GL_BLEND);
-
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	glEnable(GL_CULL_FACE);
-
-	glCullFace(GL_BACK);
-
-	glFrontFace(GL_CW);
-
 	/* build and compile our shader program */
 	// ------------------------------
-	const Shader shader("Shaders/3.1.blending.vs", "Shaders/3.1.blending.fs");
+	const Shader shader("Shaders/5.1.framebuffers.vs", "Shaders/5.1.framebuffers.fs");
+
+	const Shader screenShader("Shaders/5.1.framebuffers_screen.vs", "Shaders/5.1.framebuffers_screen.fs");
 
 	/* set up vertex data (and buffer(s)) and configure vertex attributes */
 	// ------------------------------
-	/*
-	 * Remember: to specify vertices in a counter-clockwise winding order you need to visualize the triangle
-	 * as if you're in front of the triangle and from that point of view, is where you set their order.
-	 * To define the order of a triangle on the right side of the cube for example, you'd imagine yourself looking
-	 * straight at the right side of the cube, and then visualize the triangle and make sure their order is specified
-	 * in a counter-clockwise order. This takes some practice, but try visualizing this yourself and see that this is correct.
-	 */
 	float cubeVertices[] = {
-		/* Back face */
+		/* positions */ /* texture Coords */
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, /* Bottom-left */
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
-		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, /* bottom-right */
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, /* bottom-left */
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, /* top-left */
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
 
-		/* Front face */
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-left */
-		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, /* bottom-right */
-		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, /* top-right */
-		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, /* top-right */
-		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, /* top-left */
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-left */
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-		/* Left face */
-		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-right */
-		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-left */
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-left */
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-left */
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-right */
-		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-right */
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-		/* Right face */
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-left */
-		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-right */
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
-		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-right */
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-left */
-		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-left */
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
 
-		/* Bottom face */
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* top-right */
-		0.5f, -0.5f, -0.5f, 1.0f, 1.0f, /* top-left */
-		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, /* bottom-left */
-		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, /* bottom-left */
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-right */
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* top-right */
-
-		/* Top face */
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, /* top-left */
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* bottom-right */
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* bottom-right */
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, /* top-left */
-		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f /* bottom-left */
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
 	};
 
 	float planeVertices[] = {
@@ -175,16 +151,16 @@ int main()
 		5.0f, -0.5f, -5.0f, 2.0f, 2.0f
 	};
 
-	float transparentVertices[] = {
-		/* positions */
-		/* texture Coords (swapped y coordinates because texture is flipped upside down) */
-		0.0f, 0.5f, 0.0f, 0.0f, 0.0f,
-		0.0f, -0.5f, 0.0f, 0.0f, 1.0f,
-		1.0f, -0.5f, 0.0f, 1.0f, 1.0f,
+	/* vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates. */
+	float quadVertices[] = {
+		/* positions */ /* texCoords */
+		-1.0f, 1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, 0.0f, 0.0f,
+		1.0f, -1.0f, 1.0f, 0.0f,
 
-		0.0f, 0.5f, 0.0f, 0.0f, 0.0f,
-		1.0f, -0.5f, 0.0f, 1.0f, 1.0f,
-		1.0f, 0.5f, 0.0f, 1.0f, 0.0f
+		-1.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 1.0f, 1.0f
 	};
 
 	/* cube VAO */
@@ -208,8 +184,6 @@ int main()
 
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
 
-	glBindVertexArray(0);
-
 	/* plane VAO */
 	unsigned int planeVAO, planeVBO;
 
@@ -231,53 +205,90 @@ int main()
 
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
 
-	/* transparent VAO */
-	unsigned int transparentVAO, transparentVBO;
+	/* screen quad VAO */
+	unsigned int quadVAO, quadVBO;
 
-	glGenVertexArrays(1, &transparentVAO);
+	glGenVertexArrays(1, &quadVAO);
 
-	glGenBuffers(1, &transparentVBO);
+	glGenBuffers(1, &quadVBO);
 
-	glBindVertexArray(transparentVAO);
+	glBindVertexArray(quadVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), static_cast<void*>(nullptr));
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), static_cast<void*>(nullptr));
 
 	glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
-
-	glBindVertexArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
 
 	/* load textures */
 	// ------------------------------
-	const auto cubeTexture = loadTexture("Textures/marble.jpg");
+	const auto cubeTexture = loadTexture("Textures/container.jpg");
 
 	const auto floorTexture = loadTexture("Textures/metal.png");
-
-	const auto transparentTexture = loadTexture("Textures/blending_transparent_window.png");
-
-	/* transparent window locations */
-	// ------------------------------
-	std::vector<glm::vec3> windows
-	{
-		glm::vec3(-1.5f, 0.0f, -0.48f),
-		glm::vec3(1.5f, 0.0f, 0.51f),
-		glm::vec3(0.0f, 0.0f, 0.7f),
-		glm::vec3(-0.3f, 0.0f, -2.3f),
-		glm::vec3(0.5f, 0.0f, -0.6f)
-	};
 
 	/* shader configuration */
 	// ------------------------------
 	shader.use();
 
 	shader.setInt("texture0", 0);
+
+	screenShader.use();
+
+	screenShader.setInt("screenTexture", 0);
+
+	/* framebuffer configuration */
+	// ------------------------------
+	unsigned int framebuffer;
+
+	glGenFramebuffers(1, &framebuffer);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+	/* create a color attachment texture */
+	unsigned int textureColorbuffer;
+
+	glGenTextures(1, &textureColorbuffer);
+
+	glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, scr_with, scr_height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+
+	/* create a renderbuffer object for depth and stencil attachment (we won't be sampling these) */
+
+	unsigned int rbo;
+
+	glGenRenderbuffers(1, &rbo);
+
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+
+	/* use a single renderbuffer object for both a depth AND stencil buffer. */
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, scr_with, scr_height);
+
+	/* now actually attach it */
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+	/* now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now */
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	/* draw as wireframe */
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	/* render loop */
 	// ------------------------------
@@ -295,24 +306,19 @@ int main()
 		// ------------------------------
 		process_input(window);
 
-		/* sort the transparent windows before rendering */
-		// ------------------------------
-		std::map<float, glm::vec3> sorted;
-
-		for (auto win : windows)
-		{
-			auto distance = length(camera.Position - win);
-
-			sorted[distance] = win;
-		}
-
 		/* render */
 		// ------------------------------
+		/* bind to framebuffer and draw scene as we normally would to color texture  */
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+		/* enable depth testing (is disabled for rendering screen-space quad) */
+		glEnable(GL_DEPTH_TEST);
+
+		/* make sure we clear the framebuffer's content */
 		glClearColor(0.1f, 0.1f, 0.1f, 1.f);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		/* draw objects */
 		shader.use();
 
 		auto model = glm::mat4(1.0f);
@@ -359,25 +365,27 @@ int main()
 
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-		/* windows (from furthest to nearest) */
-		glBindVertexArray(transparentVAO);
+		glBindVertexArray(0);
 
-		glBindTexture(GL_TEXTURE_2D, transparentTexture);
+		/* now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture */
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-		shader.setMat4("view", view);
+		/* disable depth test so screen-space quad isn't discarded due to depth test. */
+		glDisable(GL_DEPTH_TEST);
 
-		shader.setMat4("projection", projection);
+		/* set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways) */
+		glClearColor(1.f, 1.f, 1.f, 1.f);
 
-		for (auto it = sorted.rbegin(); it != sorted.rend(); ++it)
-		{
-			model = glm::mat4(1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-			model = translate(model, it->second);
+		screenShader.use();
 
-			shader.setMat4("model", model);
+		glBindVertexArray(quadVAO);
 
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-		}
+		/* use the color attachment texture as the texture of the quad plane */
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		/* glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.) */
 		// ------------------------------
@@ -385,6 +393,20 @@ int main()
 
 		glfwPollEvents();
 	}
+
+	/* optional: de-allocate all resources once they've outlived their purpose: */
+	// ------------------------------
+	glDeleteVertexArrays(1, &cubeVAO);
+
+	glDeleteVertexArrays(1, &planeVAO);
+
+	glDeleteVertexArrays(1, &quadVAO);
+
+	glDeleteBuffers(1, &cubeVBO);
+
+	glDeleteBuffers(1, &planeVBO);
+
+	glDeleteBuffers(1, &quadVBO);
 
 	/* glfw: terminate, clearing all previously allocated GLFW resources. */
 	// ------------------------------
@@ -501,9 +523,9 @@ unsigned int loadTexture(char const* path)
 
 		glGenerateMipmap(GL_TEXTURE_2D);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 

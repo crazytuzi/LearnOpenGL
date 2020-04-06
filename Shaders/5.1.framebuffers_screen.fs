f947454926c8731a485727a5808dfd5a4e6531d9
crazytuzi <957824770@qq.com>
2020-04-06 19:01:15 +0800

    Kernel effects

--- Shaders/5.1.framebuffers_screen.fs
+++ Shaders/5.1.framebuffers_screen.fs
@@ -6,11 +6,41 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1.f / 300.f;
+
 void main()
 {
-    FragColor = texture(screenTexture, TexCoords);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset, offset), /* top-left */  
+        vec2(0.f, offset), /* top-center */  
+        vec2(offset, offset), /* top-right */  
+        vec2(-offset, 0.f), /* center-left */  
+        vec2(0.f, 0.f), /* center-center */  
+        vec2(offset, 0.f), /* center-right */  
+        vec2(-offset, -offset), /* bottom-left */  
+        vec2(0.f, -offset), /* bottom-center */  
+        vec2(offset, -offset) /* bottom-right */
+    );
 
-    float average = 0.2116 * FragColor.r + 0.7152 * FragColor.g + 0.0722* FragColor.b;
+    float kernel[9] = float[](
+        -1,     -1,     -1,
+        -1,     9,      -1,
+        -1 ,    -1,     -1
+    );
 
-    FragColor = vec4(average, average, average, 1.0);
+    vec3 sampleTex[9];
+
+    for(int i = 0; i < 9; ++i)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+
+    vec3 col = vec3(0.f);
+
+    for(int i =0; i < 9; ++i)
+    {
+        col += sampleTex[i] * kernel[i];
+    }
+
+    FragColor = vec4(col, 1.0);
 } 

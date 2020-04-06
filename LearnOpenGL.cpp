78632d54f830af664044fd058d94659693b09790
crazytuzi <957824770@qq.com>
2020-04-06 15:37:56 +0800

    Face culling

--- LearnOpenGL.cpp
+++ LearnOpenGL.cpp
@@ -94,55 +94,74 @@ int main()
 
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	glEnable(GL_CULL_FACE);
+
+	glCullFace(GL_BACK);
+
+	glFrontFace(GL_CW);
+
 	/* build and compile our shader program */
 	// ------------------------------
 	const Shader shader("Shaders/3.1.blending.vs", "Shaders/3.1.blending.fs");
 
 	/* set up vertex data (and buffer(s)) and configure vertex attributes */
 	// ------------------------------
+	/*
+	 * Remember: to specify vertices in a counter-clockwise winding order you need to visualize the triangle
+	 * as if you're in front of the triangle and from that point of view, is where you set their order.
+	 * To define the order of a triangle on the right side of the cube for example, you'd imagine yourself looking
+	 * straight at the right side of the cube, and then visualize the triangle and make sure their order is specified
+	 * in a counter-clockwise order. This takes some practice, but try visualizing this yourself and see that this is correct.
+	 */
 	float cubeVertices[] = {
-		/* positions */ /* texture Coords */
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+		/* Back face */
 
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, /* Bottom-left */
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, /* bottom-right */
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, /* bottom-left */
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, /* top-left */
 
-		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		/* Front face */
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-left */
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, /* bottom-right */
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, /* top-right */
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, /* top-right */
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, /* top-left */
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-left */
 
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		/* Left face */
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-right */
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-left */
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-left */
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-left */
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-right */
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-right */
 
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		/* Right face */
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-left */
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-right */
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* bottom-right */
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* top-left */
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-left */
 
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+		/* Bottom face */
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* top-right */
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f, /* top-left */
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, /* bottom-left */
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, /* bottom-left */
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, /* bottom-right */
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, /* top-right */
+
+		/* Top face */
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, /* top-left */
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* bottom-right */
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, /* top-right */
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, /* bottom-right */
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, /* top-left */
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f /* bottom-left */
 	};
 
 	float planeVertices[] = {

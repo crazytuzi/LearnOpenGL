9aaedd8c80cabc6f3125dd0cb77ff3b80a1a7929
crazytuzi <957824770@qq.com>
2020-04-06 18:42:25 +0800

    Inversion

--- Shaders/5.1.framebuffers_screen.fs
+++ Shaders/5.1.framebuffers_screen.fs
@@ -8,7 +8,7 @@ uniform sampler2D screenTexture;
 
 void main()
 {
-    vec3 col = texture(screenTexture, TexCoords).rgb;
+    vec3 col = 1.f- texture(screenTexture, TexCoords).rgb;
 
     FragColor = vec4(col, 1.0);
 } 
